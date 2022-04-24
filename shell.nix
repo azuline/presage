324743fb724be108893c2a0f726dea5f29640365
blissful <blissful@sunsetglow.net>
2022-04-23 22:46:09 -0400

    add deps

--- shell.nix
+++ shell.nix
@@ -16,4 +16,17 @@ let
 in
 pkgs.mkShell {
   buildInputs = [ env ];
+  shellHook = ''
+    # Isolate build stuff to this repo's directory.
+
+    export PRESAGE_ROOT="$(pwd)"
+    export PRESAGE_CACHE_ROOT="$(pwd)/.cache"
+
+    export GOCACHE="$PRESAGE_CACHE_ROOT/go/cache"
+    export GOENV="$PRESAGE_CACHE_ROOT/go/env"
+    export GOPATH="$PRESAGE_CACHE_ROOT/go/path"
+    export GOMODCACHE="$GOPATH/pkg/mod"
+    export GOROOT=
+    export PATH=$(go env GOPATH)/bin:$PATH
+  '';
 }

00e99632d895ff59101274d59ab20e775fe45f1c
blissful <blissful@sunsetglow.net>
2022-04-23 22:50:32 -0400

    add sqlite dep and flag reads

--- cmd/presage/main.go
+++ cmd/presage/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hello!")
+
+	// Read CLI flags.
+	configPath := flag.String("config-path", "", "path to RSS feeds")
+	sendTo := flag.String("send-to", "", "email to send to")
+	// Read environment variables.
+	sendgridKey := os.Getenv("SENDGRID_KEY")
 }

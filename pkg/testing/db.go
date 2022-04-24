5def32c70a496fbb0f4a299775dbf92159c03be5
blissful <blissful@sunsetglow.net>
2022-04-24 02:32:29 -0400

    add testing services

---
+++ pkg/testing/db.go
@@ -0,0 +1,44 @@
+package testing
+
+import (
+	"database/sql"
+	"path/filepath"
+	"testing"
+
+	"github.com/azuline/presage/pkg/email"
+	"github.com/azuline/presage/pkg/migrate"
+	"github.com/azuline/presage/pkg/services"
+	"github.com/jmoiron/sqlx"
+	"github.com/stretchr/testify/require"
+)
+
+func Services(t *testing.T) *services.Services {
+	// Database.
+	testdir := t.TempDir()
+	databasePath := filepath.Join(testdir, "presage.sqlite")
+
+	plainDB, err := sql.Open("sqlite", databasePath)
+	require.NoError(t, err)
+
+	db := sqlx.NewDb(plainDB, "sqlite")
+
+	// Email.
+	smtpCreds := email.SMTPCreds{
+		User: "username@ema.il",
+		Pass: "hopeyouknowit",
+		Host: "lol.find.your.own",
+		Port: "465",
+	}
+	emailClient := email.NewClient(smtpCreds)
+
+	// Migrate database.
+	srv := &services.Services{
+		PlainDB: plainDB,
+		DB:      db,
+		Email:   emailClient,
+	}
+	err = migrate.Migrate(srv)
+	require.NoError(t, err)
+
+	return srv
+}
